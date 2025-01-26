00fca6b3a654d6d9f53eaf9a2f5148cb2fafd871
Christine Ramos <tineramos.ph@gmail.com>
2025-01-26 01:28:29 +0000

    Organise assets and other resources

---
+++ LJPBucketList/MenuTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuTabView.swift
+//  LJPBucketList
+//
+//  Created by Tine Ramos on 19/01/2025.
+//
+
+import SwiftUI
+
+struct MenuTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuTabView()
+}
