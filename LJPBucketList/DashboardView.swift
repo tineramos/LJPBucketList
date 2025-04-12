907865ed9994167a52c741e2a026dc6d5294aee9
Christine Ramos <tineramos.ph@gmail.com>
2025-04-12 04:28:05 +0100

    Configure Dashboard Menu Button

--- LJPBucketList/DashboardView.swift
+++ LJPBucketList/DashboardView.swift
@@ -8,7 +8,48 @@
 import SwiftUI
 import SwiftData
 
+private let gridItemSize: CGFloat = 150
+private let gridItemSpacing: CGFloat = 20
+
 struct DashboardView: View {
+	
+	enum Menu: String, CaseIterable {
+		case bucketList
+		case completed
+		case rules
+		case instagram
+		
+		var titleIcon: MenuButton.TitleIcon {
+			switch self {
+			case .bucketList:
+				return .text("422")
+			case .completed:
+				return .text("31")
+			case .rules:
+				return .image(.Dashboard.rulesIcon)
+			case .instagram:
+				return .image(.Dashboard.instagramIcon)
+			}
+		}
+		
+		var title: String {
+			switch self {
+			case .bucketList:
+				return "Bucket List"
+			case .completed:
+				return "Completed"
+			case .rules:
+				return "Rules"
+			case .instagram:
+				return "Instagram"
+			}
+		}
+	}
+
+	private let fixedColumn = [
+		GridItem(.fixed(gridItemSize), spacing: gridItemSpacing),
+		GridItem(.fixed(gridItemSize), spacing: gridItemSpacing)
+	]
 
     var body: some View {
 		NavigationStack {
@@ -21,7 +62,7 @@ struct DashboardView: View {
 					VStack(spacing: 16) {
 						logoHeader
 						coverImage
-						
+						contentActions(width: proxy.size.width)
 						Spacer()
 					}
 				}
@@ -51,30 +92,69 @@ struct DashboardView: View {
 			.frame(alignment: .center)
 			.aspectRatio(contentMode: .fit)
 	}
+	
+	private func contentActions(width: CGFloat) -> some View {
+		ScrollView {
+			LazyVGrid(columns: fixedColumn, alignment: .center, spacing: gridItemSpacing) {
+				ForEach(Menu.allCases, id: \.self) { item in
+					MenuButton(menuItem: item)
+				}
+			}
+			.frame(maxWidth: .infinity)
+		}
+		.scrollBounceBehavior(.basedOnSize)
+	}
 }
 
 struct MenuButton: View {
 	
-	let title: String
-	let action: () -> Void
+	enum TitleIcon {
+		case text(String)
+		case image(ImageResource)
+	}
 	
+	let menuItem: DashboardView.Menu
+	
+//	let title: String
+//	let action: () -> Void
+
 	var body: some View {
-		
-		Button {
-			action()
-		} label: {
-			
-			Text(title)
-				.fontWeight(.bold)
-				.padding()
-				.frame(maxWidth: .infinity)
-				.foregroundStyle(.accent)
-				.background(
-					RoundedRectangle(cornerRadius: 5,
-									 style: .continuous)
-						.stroke(lineWidth: 3))
-			
+		Button(action: {
+			print("TINE")
+		}) {
+			VStack {
+				switch menuItem.titleIcon {
+				case .text(let string):
+					Text(string)
+						.foregroundStyle(Color.textBlackForeground)
+						.font(.largeTitle.bold())
+						.padding(.bottom, 8)
+				case .image(let imageResource):
+					Image(imageResource)
+						.foregroundStyle(Color.textGreenForeground)
+				}
+				Text(menuItem.title)
+					.font(.title2.bold())
+			}
 		}
+		.frame(width: gridItemSize, height: gridItemSize)
+		.background(
+			LinearGradient(
+				stops: [
+					.init(color: .white.opacity(0.3), location: 0),
+					.init(color: .dashboardBackground.opacity(0.7), location: 1),
+				],
+				startPoint: .topLeading,
+				endPoint: .bottomTrailing
+			)
+		)
+		.cornerRadius(20)
+		.overlay(
+			RoundedRectangle(cornerRadius: 20)
+				.inset(by: 0.5)
+				.stroke(Color.white, lineWidth: 0.2)
+		)
+			
 		
 	}
 }
