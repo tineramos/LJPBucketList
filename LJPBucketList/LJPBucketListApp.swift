fc93bb6076b6cb792d78de7afc8bf2e9a1314546
Christine Ramos <tineramos.ph@gmail.com>
2025-05-29 12:12:52 +0100

    Delete SwiftData stuff

--- LJPBucketList/LJPBucketListApp.swift
+++ LJPBucketList/LJPBucketListApp.swift
@@ -10,21 +10,10 @@ import SwiftData
 
 @main
 struct LJPBucketListApp: App {
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
 
     var body: some Scene {
         WindowGroup {
 			MenuTabView()
         }
-//        .modelContainer(sharedModelContainer)
     }
 }
